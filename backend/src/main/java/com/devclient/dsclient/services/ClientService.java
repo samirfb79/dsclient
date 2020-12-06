24dccc26f9126a3d6f80b287aea315a235dfa66e
Samir Baracat <samirbaracat@hotmail.com>
2020-12-06 10:39:04 -0400

    Update Client

--- backend/src/main/java/com/devclient/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devclient/dsclient/services/ClientService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devclient.dsclient.dto.ClientDTO;
 import com.devclient.dsclient.entities.Client;
 import com.devclient.dsclient.repositories.ClientRepository;
-import com.devclient.dsclient.services.exceptions.EntityNotFoundException;
+import com.devclient.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -28,7 +30,7 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
-		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity Not Found"));
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity Not Found"));
 		return new ClientDTO(entity);
 	}
 
@@ -42,6 +44,23 @@ public class ClientService {
 		entity.setChildren(dto.getChildren());
 		entity = repository.save(entity);
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setCpf(dto.getCpf());
+			entity.setIncome(dto.getIncome());
+			entity.setBirthDate(dto.getBirthDate());
+			entity.setChildren(dto.getChildren());
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 
 }
