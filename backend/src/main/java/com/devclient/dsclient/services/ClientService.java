ae5aeda453f252b58d76cd73985ce1778848bc75
Samir Baracat <samirbaracat@hotmail.com>
2020-12-08 10:50:51 -0400

    Delete Client

--- backend/src/main/java/com/devclient/dsclient/services/ClientService.java
+++ backend/src/main/java/com/devclient/dsclient/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devclient.dsclient.dto.ClientDTO;
 import com.devclient.dsclient.entities.Client;
 import com.devclient.dsclient.repositories.ClientRepository;
+import com.devclient.dsclient.services.exceptions.DatabaseException;
 import com.devclient.dsclient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,6 +64,19 @@ public class ClientService {
 		catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
 		}
+	}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+		
 	}
 
 }
