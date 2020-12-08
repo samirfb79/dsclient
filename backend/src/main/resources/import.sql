9fe75c5aa7364c7aa14dd4f2a8f5979056a80ca3
Samir Baracat <samirbaracat@hotmail.com>
2020-12-08 11:05:58 -0400

    Auditiong data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('MARIA', '11111111111', 50, '2001-11-17T14:22:40Z', 2);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('CARLOS', '22222222222', 18, '2004-12-11T22:11:22Z', 1);
-INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('EDUARDO', '33333333333', 37, '2007-10-28T19:18:14Z', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('MARIA', '11111111111', 50, '2001-11-17T14:22:40Z', 2, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('CARLOS', '22222222222', 18, '2004-12-11T22:11:22Z', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('EDUARDO', '33333333333', 37, '2007-10-28T19:18:14Z', 4, NOW());
