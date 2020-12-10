131c428957e6c1415c6d9a9734344ee9ac43ec6d
Samir Baracat <samirbaracat@hotmail.com>
2020-12-10 10:43:47 -0400

    Pagination

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,6 @@
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('MARIA', '11111111111', 50, '2001-11-17T14:22:40Z', 2, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('CARLOS', '22222222222', 18, '2004-12-11T22:11:22Z', 1, NOW());
 INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('EDUARDO', '33333333333', 37, '2007-10-28T19:18:14Z', 4, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('JOSE', '12312211122', 28, '2012-12-14T11:23:20Z', 1, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('JOAO', '56985632147', 19, '2009-10-19T17:17:28Z', 3, NOW());
+INSERT INTO tb_client (name, cpf, income, birth_date, children, created_at) VALUES ('FABIO', '55632698563', 25, '2008-11-21T14:15:17Z', 5, NOW());
